505418c9583dc3a92fc52bc4f1a304dfd8d47b3c
Fredrik Pedersen <fredrik.pedersen@norgesgruppen.no>
2023-09-10 21:12:21 +0200

    Made bar widht responsive

--- frontend/src/containers/leaderboard/Leaderboard.tsx
+++ frontend/src/containers/leaderboard/Leaderboard.tsx
@@ -59,15 +59,15 @@ export function Leaderboard() {
     }, [])
 
     return (
-        <div style={{width: "100%", height: "100%"}}>
+        <ResponsiveContainer width={"100%"} height={500}>
             <BarChart
                 width={500}
                 height={300}
                 data={drinkStats}
                 margin={{
                     top: 20,
-                    right: 30,
-                    left: 20,
+                    right: 40,
+                    left: 0,
                     bottom: 5,
                 }}
             >
@@ -82,6 +82,6 @@ export function Leaderboard() {
                     )
                 })}
             </BarChart>
-        </div>
+        </ResponsiveContainer>
     );
 }
