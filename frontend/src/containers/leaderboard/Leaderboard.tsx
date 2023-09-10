eae727e1114b9dfb8a521c4e469d4045635a83d6
Fredrik Pedersen <fredrik.pedersen@norgesgruppen.no>
2023-09-10 13:33:15 +0200

    Implemented logic for creating drink leaderboard statistics

--- frontend/src/containers/leaderboard/Leaderboard.tsx
+++ frontend/src/containers/leaderboard/Leaderboard.tsx
@@ -1,35 +1,65 @@
 import {Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis} from "recharts";
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import {OrderContext} from "@context/OrderContext.tsx";
 
-export function Leaderboard() {
-    const {orders, drinks} = useContext(OrderContext);
+type DrinkQuantity = {
+    drinkName: string,
+    quantity: number
+}
 
-    //Find unique usernames
-    const usernames = orders.map(drinkOrder => drinkOrder.username);
+type DrinkStats = {
+    username: string,
+    drinkQuantities: DrinkQuantity[]
+}
+
+export function Leaderboard() {
+    const {orders} = useContext(OrderContext);
+    const [drinkStats, setDrinkStats] = useState<DrinkStats[]>([])
 
     //Foreach username, find the occurrence of each drink per username
-    usernames.forEach(username => {
-        const drinkForUsername = orders.map(drinkOrder => {
-            if (drinkOrder.username === username) {
+     useEffect(()  => {
+        //Find unique usernames
+        const usernames = [...new Set(orders.map(drinkOrder => drinkOrder.username))];
 
-            }
+        const drinksPerUser = usernames.map(username => {
+            const statsForUser: DrinkStats = {
+                username: username,
+                drinkQuantities: []
+            };
+
+            orders.forEach(drinkOrder => {
+                if (drinkOrder.username === username) {
+                    const drinkQuantitiesForUser = statsForUser.drinkQuantities
+                    const drinkExists = drinkQuantitiesForUser.find((drink) => drinkOrder.drinkName === drink.drinkName)
+                    if (drinkExists) {
+                        drinkExists.quantity += 1
+                    } else {
+                        drinkQuantitiesForUser.push({
+                            drinkName: drinkOrder.drinkName,
+                            quantity: 1
+                        })
+                    }
+                }
+            })
+
+            return statsForUser;
         })
 
-        console.log(drinkForUsername)
-    })
+         setDrinkStats(drinksPerUser)
+    }, [orders])
 
 
+    console.log(drinkStats)
     return (
         <>
-            <BarChart width={300} height={500} data={orders}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="pv" fill="#8884d8" />
-                <Bar dataKey="uv" fill="#82ca9d" />
+            <BarChart width={300} height={500} data={drinkStats}>
+                <CartesianGrid strokeDasharray="3 3"/>
+                <XAxis dataKey="name"/>
+                <YAxis/>
+                <Tooltip/>
+                <Legend/>
+                <Bar dataKey="pv" fill="#8884d8"/>
+                <Bar dataKey="uv" fill="#82ca9d"/>
             </BarChart>
         </>
     )
