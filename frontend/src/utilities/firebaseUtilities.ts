28b1ecd3c81b839ed9371695c3d03d99bbe28650
Fredrik Pedersen <fredrikhp@gmail.com>
2023-09-26 21:10:32 +0200

    Added utility function for creating Drinkorder Docs

--- frontend/src/utilities/firebaseUtilities.ts
+++ frontend/src/utilities/firebaseUtilities.ts
@@ -1,5 +1,11 @@
 import {QuerySnapshot} from '@firebase/firestore-types';
+import {doc} from "firebase/firestore";
+import {firestore} from "@/config/firebase.ts";
 
 export const mapDocumentData = <T>(documentResponse: QuerySnapshot) => {
-    return documentResponse.docs.map(doc => doc.data() as T)
+    return documentResponse.docs.map(doc => ({...doc.data(), id: doc.id}) as T)
+}
+
+export const createOrderDoc = (id: string) => {
+    return doc(firestore, "order", id)
 }
