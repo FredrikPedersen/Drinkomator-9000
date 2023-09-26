5f775ff4e64b7bcc0a79cd8d5af432a43930be98
Fredrik Pedersen <fredrikhp@gmail.com>
2023-09-26 21:10:48 +0200

    Added functionality for setting isDone on Drinkorders

--- frontend/src/containers/queue/Queue.tsx
+++ frontend/src/containers/queue/Queue.tsx
@@ -1,9 +1,9 @@
 import {Button, Table} from "react-bootstrap";
-import {useEffect, useMemo, useState} from "react";
+import {useCallback, useEffect, useMemo, useState} from "react";
 import {DrinkOrder} from "@models/drinkOrder.ts";
 import {orderCollection} from "@/config/firebase.ts";
-import {getDocs} from "firebase/firestore";
-import {mapDocumentData} from "@utilities/firebaseUtilities.ts";
+import {getDocs, updateDoc} from "firebase/firestore";
+import {createOrderDoc, mapDocumentData} from "@utilities/firebaseUtilities.ts";
 import {ROLE, User, USER_LS_KEY} from "@models/user.ts";
 
 export function Queue() {
@@ -20,19 +20,29 @@ export function Queue() {
 
     //TODO Can we move the isDone part of the query to Firebase in order to reduce size of HTTP payload and clientside workload?
     useEffect(() => {
-        const getOrders = async () => {
-            const orderQuery = await getDocs(orderCollection);
-            const orders: DrinkOrder[] = mapDocumentData<DrinkOrder>(orderQuery);
-
-            return orders
-                .filter(drinkOrder => !drinkOrder.isDone)
-                .sort((a, b) => {
-                return a.createdDate - b.createdDate
-            });
-        }
-
         getOrders().then(sorted => setSortedOrders(sorted));
     }, []);
+
+    const markAsDone = useCallback(async (id: string) => {
+        const drinkOrderDoc = createOrderDoc(id);
+        const newField = {isDone: true}
+        updateDoc(drinkOrderDoc, newField)
+            .then(() => {
+                getOrders()
+                    .then(sorted => setSortedOrders(sorted))
+            })
+    }, [])
+
+    const getOrders = useCallback(async () => {
+        const orderQuery = await getDocs(orderCollection);
+        const orders: DrinkOrder[] = mapDocumentData<DrinkOrder>(orderQuery);
+
+        return orders
+            .filter(drinkOrder => !drinkOrder.isDone)
+            .sort((a, b) => {
+                return a.createdDate - b.createdDate
+            });
+    }, [])
 
     return (
         <Table striped bordered hover>
@@ -56,7 +66,7 @@ export function Queue() {
                         <td>{queueRow.drinkName}</td>
                         {isAdmin ?
                                 <td>
-                                    <Button variant="success" style={{width: "100%"}}>Done</Button>
+                                    <Button variant="success" style={{width: "100%"}} onClick={() => markAsDone(queueRow.id)}>Done</Button>
                                     <Button variant="danger" style={{width: "100%"}}>Delete</Button>
                                 </td>
                             : null
