654a780a163d220433e2678f1641a0bb8e639129
Fredrik Pedersen <fredrikhp@gmail.com>
2023-09-28 18:52:23 +0200

    Created custom hook for setting up polling

---
+++ frontend/src/hooks/usePolling.ts
@@ -0,0 +1,24 @@
+import {useEffect, useRef} from "react";
+
+export function useInterval(callback: Function, delay: number = 120000) {
+    const savedCallback = useRef<Function>()
+
+    useEffect(() => {
+        savedCallback.current = callback
+    }, [callback]);
+
+    useEffect(() => {
+        function tick() {
+            if (savedCallback && savedCallback.current) {
+                savedCallback.current()
+            }
+        }
+
+        if (delay !== null) {
+            const id = setInterval(tick, delay)
+            return () => {
+                clearInterval(id)
+            }
+        }
+    }, [callback, delay]);
+}
