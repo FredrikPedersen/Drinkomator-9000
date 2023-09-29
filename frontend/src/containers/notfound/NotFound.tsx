2c79911585b492d0efdf8d4047f032aeead47979
Fredrik Pedersen <fredrikhp@gmail.com>
2023-09-29 19:52:22 +0200

    Spiced up the error component

--- frontend/src/containers/notfound/NotFound.tsx
+++ frontend/src/containers/notfound/NotFound.tsx
@@ -1,18 +1,44 @@
-import { useRouteError } from "react-router-dom";
+import {isRouteErrorResponse, useRouteError} from "react-router-dom";
 import './NotFound.css'
+import {useEffect, useMemo, useState} from "react";
+import {User, USER_LS_KEY} from "@models/user.ts";
 
 export default function NotFound() {
     const error = useRouteError();
+    const [user, setUser] = useState<User | undefined>(undefined)
+
+    useEffect(() => {
+        if (!user) {
+            const userAsString = localStorage.getItem(USER_LS_KEY)
+
+            if (userAsString) {
+                setUser(JSON.parse(userAsString))
+            }
+        }
+    }, [user]);
+
+
+    const errorMessage = useMemo(() => {
+        if (isRouteErrorResponse(error)) {
+            // error is type `ErrorResponse`
+            return error.error?.message || error.statusText;
+        } else if (error instanceof Error) {
+            return error.message;
+        } else if (typeof error === 'string') {
+            return error;
+        } else {
+            console.error(error);
+            return 'Unknown error';
+        }
+    }, [error])
+
 
     return (
         <div id="not-found">
-            <h1>HVA FAEN ER DET DU DRIVER MED!?</h1>
-            {error.statusText === "Not Found"
-                ? <p>Denna siden eksisterer ikke. Pell deg tilbake til landingssiden og ta en drink til!</p>
-                : <p>
-                    <i>{error.statusText || error.message}</i>
-                </p>
-            }
+            <h1>{`I'm afraid I can't do that, ${user ? user.username : 'Dave'}`}</h1>
+            <p>
+                <i>{errorMessage}</i>
+            </p>
         </div>
     );
 }
